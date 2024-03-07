0c5b7a98140d6bc890d3f9895981b587bc287963
jonghechoi <sosinnmi@naver.com>
2024-03-08 00:51:47 +0900

    feat(infra): 젠킨스 파이프라인 테스트
    최종희

--- src/test/java/com/example/netflixeureka/NetflixEurekaApplicationTests.java
+++ src/test/java/com/example/netflixeureka/NetflixEurekaApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class NetflixEurekaApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
